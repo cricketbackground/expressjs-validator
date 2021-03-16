f7b4481c1c8853f7243288d7f8f2d9449c66db71
cricketbackground <vadzz.java@gmail.com>
2021-03-16 05:52:30 -0400

    multer in memory storage approach

--- routes/upload.js
+++ routes/upload.js
@@ -12,14 +12,18 @@ const XLSX = require('xlsx');
 
 // The uploading will be completed once the middleware returns, i.e. once this route is hit.
 router.post('/', upload.single('orders_request'), function (req, res) {
-  // req.file is the name of your file in the form above, here 'orders_request'
-  // console.log("req.file ==> ", req.file);
   var workbook = XLSX.read(req.file.buffer);
   let ordersSheetName = workbook.SheetNames[0];
   var worksheet = workbook.Sheets[ordersSheetName];
   let json = XLSX.utils.sheet_to_json(worksheet);
   console.log("json ", json);
-  res.json(json);
+  if (json.length === 0) {
+    res.json({
+      message: "file is empty!"
+    });
+  } else {
+    res.json(json);
+  }
 });
 
 module.exports = router;
