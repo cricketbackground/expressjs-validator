2f7b61ea69111d2e2ad0ee8c5dc6bcbfb666cce4
cricketbackground <vadzz.java@gmail.com>
2021-03-16 05:41:41 -0400

    multer in memory storage approach

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const lessMiddleware = require('less-middleware');
+const logger = require('morgan');
+
 const indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var uploadRouter = require('./routes/upload');
-var bodyParser = require('body-parser')
+const upload = require('./routes/upload');
 
 const app = express();
-const port = 3000;
-
-//Tell Express.js where to find your Javascript files.
-// parse application/json
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//Mount routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/upload', uploadRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Expressjs-Validator listening on port ${port}!`));
+app.use('/', indexRouter);
+app.use('/upload', upload);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/upload.js
+++ routes/upload.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var router = express.Router();
-var multer = require('multer')
-var upload = multer({dest: './public/data/uploads/'})
-var readXlsxFile = require('xlsx');
-var fs = require('fs');
-var {promisify} = require('util')
-var unlinkAsync = promisify(fs.unlink)
+const express = require('express');
+const router = express.Router();
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  limits: {
+    fields: 1, fileSize: 10 * 1024 * 1024, files: 1, parts: 2,
+  },
+});
+const XLSX = require('xlsx');
 
 // The uploading will be completed once the middleware returns, i.e. once this route is hit.
 router.post('/', upload.single('orders_request'), function (req, res) {
   // req.file is the name of your file in the form above, here 'orders_request'
-  console.log("req.file ==> ", req.file);
-
-  var workbook = readXlsxFile.readFile(req.file.path);
+  // console.log("req.file ==> ", req.file);
+  var workbook = XLSX.read(req.file.buffer);
   let ordersSheetName = workbook.SheetNames[0];
   var worksheet = workbook.Sheets[ordersSheetName];
-  let json = readXlsxFile.utils.sheet_to_json(worksheet);
+  let json = XLSX.utils.sheet_to_json(worksheet);
   console.log("json ", json);
   res.json(json);
 });
